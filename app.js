fe4cf15c2a80635ffb4036ce58d60f46ab4d7542
mengtao wang <2726653264@qq.com>
2020-01-19 13:46:35 +0800

    http -> https

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const FileStreamRotator = require('file-stream-rotator')
 const logger = require('morgan')
 const fs = require('fs');
 const path = require('path')
+const https = require('https')
 app.use(express.json())
 
 //允许跨域
@@ -47,18 +48,35 @@ const accessLogStream = FileStreamRotator.getStream({
 
 app.use(logger('combined',{ stream: accessLogStream }));//写入日志文件
 
+const options  = {
+  key: fs.readFileSync('./ssl/2_www.kangaroo-app.cn.key'),
+  cert: fs.readFileSync('./ssl/1_www.kangaroo-app.cn_bundle.crt')
+}
 
-app.listen(3002, () =>{
-      // 6个占位符从左到右分别代表：秒、分、时、日、月、周几
-      // *表示通配符，匹配任意，当秒是*时，表示任意秒数都触发，其它类推
-      (()=>{
-        //每天的凌晨1点1分1秒触发
-        schedule.scheduleJob('1 1 1 * * *', ()=>{
-         axios.get('http://localhost:3002/user/resetActive').then(data=>{
-             console.log(data.data)
-         })
-        })  
-      })();
-    console.log('3002 port is opened')
-   
-})
+// app.listen(3002, () =>{
+//       // 6个占位符从左到右分别代表：秒、分、时、日、月、周几
+//       // *表示通配符，匹配任意，当秒是*时，表示任意秒数都触发，其它类推
+//       (()=>{
+//         //每天的凌晨1点1分1秒触发
+//         schedule.scheduleJob('1 1 1 * * *', ()=>{
+//          axios.get('http://localhost:3002/user/resetActive').then(data=>{
+//              console.log(data.data)
+//          })
+//         })  
+//       })();
+//     console.log('3002 port is opened')  
+// })
+
+https.createServer(options, app).listen(3002,  () =>{
+        // 6个占位符从左到右分别代表：秒、分、时、日、月、周几
+        // *表示通配符，匹配任意，当秒是*时，表示任意秒数都触发，其它类推
+        (()=>{
+          //每天的凌晨1点1分1秒触发
+          schedule.scheduleJob('1 1 1 * * *', ()=>{
+           axios.get('http://localhost:3002/user/resetActive').then(data=>{
+               console.log(data.data)
+           })
+          })  
+        })();
+      console.log('3002 port is opened')  
+  });
